d7a8d914b9e22776e339d2037bd8c537b66b377a
Laurent Forthomme <laurent.forthomme@cern.ch>
2024-03-17 10:00:36 +0100

    Optimise Doxygen/Sphinx for Python

--- conf.in.py
+++ conf.in.py
@@ -80,7 +80,9 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [u'_build', 'README.md', 'CMakeLists.txt',  'build', 'Thumbs.db', 'requirements.txt' '.DS_Store']
+exclude_patterns = [u'_build',
+                    'README.md', 'CMakeLists.txt', 'requirements.txt', 'build', 'doxygen-awesome-css',
+                    'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 #pygments_style = 'colorful'
