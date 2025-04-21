b0541084da2a0263f4ee050e30aa0f6e42b97082
Laurent Forthomme <laurent.forthomme@cern.ch>
2025-04-21 18:04:07 +0200

    Disabling ultimate replace for now

--- conf.in.py
+++ conf.in.py
@@ -224,7 +224,7 @@ texinfo_documents = [
 ]
 
 # Substitution
-def ultimateReplace(app, docname, source):
+'''def ultimateReplace(app, docname, source):
     res = source[0]
     for key in app.config.ultimate_replacements:
         res = res.replace(key, app.config.ultimate_replacements[key])
@@ -233,7 +233,7 @@ def ultimateReplace(app, docname, source):
 from os import path
 ultimate_replacements = {
     "{CEPGEN_PATH}" : path.relpath('@CEPGEN_PATH@', path.dirname(__file__))+'/',
-}
+}'''
 
 # Breathe Configuration
 breathe_default_project = "CepGen"
@@ -249,4 +249,4 @@ sphinx_github_changelog_token = '@GH_API_TOKEN@'
 def setup(app):
     app.add_css_file('hacks.css')
     app.add_config_value('ultimate_replacements', {}, True)
-    app.connect('source-read', ultimateReplace)
+    #app.connect('source-read', ultimateReplace)
